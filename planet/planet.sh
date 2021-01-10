57a38464456f09d357a7e394b4cb042cdf783c3f
Lassi Kortela <lassi@lassi.io>
2021-01-11 01:49:19 +0200

    Reorganize home directory layout
    Put everything from the git repo into a planet/ subdirectory under the
    blog user's home directory. That way it's easier to avoid messing up
    the home directory by rsyncing stuff from the git repo.

--- planet/planet.sh
+++ planet/planet.sh
@@ -4,6 +4,8 @@ cd "$(dirname "$0")"
 [ "$(id -un)" = prod-blog ] || { echo "Run me as prod-blog"; exit 1; }
 echo "Entering directory '$PWD'"
 set -x
+rm -rf www.new/
 planet planet.ini "$@"
-rsync -vax www.new/ www/
+rsync -vax static/ www.new/
+rsync -vax www.new/ ../www/
 rm -rf www.new/
