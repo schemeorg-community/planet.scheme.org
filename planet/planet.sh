4899c291f8c00a16eef9e556faf0710336a4f85e
Lassi Kortela <lassi@lassi.io>
2021-01-11 03:00:21 +0200

    Remove final slash

--- planet/planet.sh
+++ planet/planet.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 [ "$(id -un)" = prod-blog ] || { echo "Run me as prod-blog"; exit 1; }
 echo "Entering directory '$PWD'"
 set -x
-rm -rf output/
+rm -rf output
 planet planet.ini "$@"
 rsync -vax output/ static/ ../www/
-rm -rf output/
+rm -rf output
